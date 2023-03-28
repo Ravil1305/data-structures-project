a1ab4d8f32d86f6db49ddd110e17caa77b8c1539
Ravil Nuriev <rafnuriev@mail.ru>
2023-03-28 07:04:00 +0500

    add poetry.lock, stack.py, test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack, Node
+
+class TestStack(unittest.TestCase):
+    """Тесты для модуля stack."""
+
+    def __init__(self, methodName):
+        super().__init__(methodName)
+        self.stack = None
+
+    def test_push(self):
+        """Тестируем push."""
+        stack = Stack()
+        stack.push('testdata')
+        self.assertEqual(stack.top.data, 'testdata')
+        self.assertIsInstance(stack.top, Node)
+        self.assertEqual(stack.top.data, 'testdata')
+        self.assertIs(stack.top.next_node, None)
+        stack.push('testdata1')
+        self.assertIsInstance(stack.top.next_node, Node)
+        self.assertEqual(stack.top.data, 'testdata1')
